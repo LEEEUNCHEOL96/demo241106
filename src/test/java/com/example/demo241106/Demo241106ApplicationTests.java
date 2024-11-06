530058e4fc51bb40739c7853ae5bfd5714cfd2bb
LEEEUNCHEOL96 <dmscjf1020@naver.com>
2024-11-06 09:24:16 +0900

    setting

---
+++ src/test/java/com/example/demo241106/Demo241106ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo241106;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo241106ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
